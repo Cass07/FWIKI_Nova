b7489120906f715c5039a87f6582bee4feeae4b5
Cass <kjh95828@naver.com>
2025-11-28 20:57:45 +0900

    [Refactor] BResult 갱신 시 BatchQuoteInfo status도 update하도록 수정 (#3)
    - BatchQuoteInfo status가 PENDING인 hero 데이터만 갱신함
    - 위 validation은 상위 블록에서 수행해서, PENDING인 객체만 넘겨줄 예정
    - Lock을 반남한 뒤에 status를 갱신하도록 로직 추가
    - 일부 로직을 service단에게 위임

--- src/main/java/wiki/feh/externalrestdemo/openai/batch/service/BatchQuoteInfoService.java
+++ src/main/java/wiki/feh/externalrestdemo/openai/batch/service/BatchQuoteInfoService.java
@@ -3,9 +3,11 @@ package wiki.feh.externalrestdemo.openai.batch.service;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import wiki.feh.externalrestdemo.openai.batch.domain.BatchQuoteInfo;
 import wiki.feh.externalrestdemo.openai.batch.domain.BatchQuoteInfoRepository;
+import wiki.feh.externalrestdemo.openai.batch.domain.BatchStatus;
 
 import java.util.List;
 
@@ -17,5 +19,27 @@ public class BatchQuoteInfoService {
     @Transactional
     public Mono<Void> saveAllBatchQuoteInfoList(List<BatchQuoteInfo> batchQuoteInfoList) {
         return batchQuoteInfoRepository.batchSave(batchQuoteInfoList);
+    }
+
+    @Transactional
+    public Mono<BatchQuoteInfo> findBatchQuoteInfoByHeroIdAndBatchInfoId(String heroId, int batchInfoId) {
+        return batchQuoteInfoRepository.findFirstByHeroIdAndBatchInfoId(heroId, batchInfoId);
+    }
+
+    @Transactional
+    public Flux<BatchQuoteInfo> findBatchQuoteInfoByBatchInfoId(int batchInfoId) {
+        return batchQuoteInfoRepository.findByBatchInfoId(batchInfoId);
+    }
+
+    @Transactional
+    public Mono<BatchQuoteInfo> updateBatchQuoteInfoFailed(BatchQuoteInfo batchQuoteInfo) {
+        batchQuoteInfo.updateStatus(BatchStatus.FAILED);
+        return batchQuoteInfoRepository.save(batchQuoteInfo);
+    }
+
+    @Transactional
+    public Mono<BatchQuoteInfo> updateBatchQuoteInfoComplete(BatchQuoteInfo batchQuoteInfo) {
+        batchQuoteInfo.updateStatus(BatchStatus.COMPLETED);
+        return batchQuoteInfoRepository.save(batchQuoteInfo);
     }
 }
--- src/main/java/wiki/feh/externalrestdemo/openai/bresult/facade/BResultFacade.java
+++ src/main/java/wiki/feh/externalrestdemo/openai/bresult/facade/BResultFacade.java
@@ -7,6 +7,8 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import wiki.feh.externalrestdemo.heroquote.domain.HeroQuoteKr;
 import wiki.feh.externalrestdemo.heroquote.service.HeroQuoteKrService;
+import wiki.feh.externalrestdemo.openai.batch.domain.BatchQuoteInfo;
+import wiki.feh.externalrestdemo.openai.batch.service.BatchQuoteInfoService;
 import wiki.feh.externalrestdemo.openai.bresult.dto.BResultDto;
 import wiki.feh.externalrestdemo.util.NamedLockManager;
 
@@ -16,14 +18,24 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor
 public class BResultFacade {
-    private final String HERO_QUOTE_KR_LOCK_PREFIX = "hero_quote_";
-    private final String HERO_QUOTE_KR_KIND = "Home";
-    private final int HERO_QUOTE_KR_SEQ = 1;
+    private final String HERO_QUOTE_KR_LOCK_PREFIX = "hquote_";
     private final HeroQuoteKrService heroQuoteKrService;
+    private final BatchQuoteInfoService batchQuoteInfoService;
     private final NamedLockManager namedLockManager;
 
     /*
      * flow
+     *
+     * 0>
+     * 1. batch result jsonl 수신함
+     * 2. batch result batch id를 읽어서 batchInfo 조회함 - batchid는 jsonl에 없고 데이터 다운로드 시 이미 가지고 있는걸로 써야함
+     * 3. running인지 확인 (아니면 종료)
+     * 4. findAll로 batchInfo id에 해당하는 BatchQuoteInfo 리스트 조회
+     * 5. jsonl 읽어서 필요한 데이터만 가공하면서 batchQuoteInfo 리스트와 매핑하기
+     *
+     * flux에 넘겨줘야 하는거 : BatchQuoteInfo, list of BResultDto.ApiResult
+     *
+     * 0-1. BatchQuoteInfo에 heroId로 BatchStatus.PENDING 상태인게 있는지 확인 (없으면 이하 작업은 하지 않고 종료) (상위에서 걸러서 넘겨줄것)
      * 1. jsonl을 읽어서 heroid와 BResultDto.ListRequest로 변환한 것을 받아온다 (receiveResultMock)
      * 2. heroId lock 획득
      * 3. heroId로 기존 heroQuoteKr 조회해서, 신규 인덱스 생성 (데이터 없으면 1, 아니면 ++)
@@ -31,29 +43,37 @@ public class BResultFacade {
      * 4-1. 신규 데이터가 요청 생성 후에 들어왔는지 확인해서 공개 여부 설정 구현 (일단은 무조건 공개로)
      * 5. heroQuoteKr Batch 저장
      * 6. lock 해제
+     * 7. BatchQuoteInfo 상태 COMPLETED로 변경
+     *
+     * - 상위 블록애서 BatchInfo의 상태 변경, logging 필요
+     *
+     * data가 있는데 BatchQuoteInfo가 없으면 버릴 것인가???? <-버려야지...
      */
 
     /**
      * heroId로 named lock을 획득한 후, insertHeroQuoteKr 실행
-     * @param heroId hero idx
+     * @param batchQuoteInfo batch quote info entity
      * @param results apiResult list (OpenAI response를 parsing한 것)
-     * @return
+     * @return void mono
      */
-    public Mono<Void> processAndInsertHeroQuoteKr(String heroId, List<BResultDto.ApiResult> results) {
-        String lockKey = HERO_QUOTE_KR_LOCK_PREFIX + heroId;
+    public Mono<Void> processAndInsertHeroQuoteKr(BatchQuoteInfo batchQuoteInfo, List<BResultDto.ApiResult> results) {
+        String lockKey = HERO_QUOTE_KR_LOCK_PREFIX + batchQuoteInfo.getHeroId();
         return namedLockManager.executeWithNamedLock(lockKey,
-                insertHeroQuoteKr(heroId, results)
-        );
+                insertHeroQuoteKr(batchQuoteInfo, results)
+        )
+        .then(batchQuoteInfoService.updateBatchQuoteInfoComplete(batchQuoteInfo))
+        .then();
     }
 
     /**
      * heroId로 기존 kr 데이터를 조회해서, 신규 인덱스를 생성하여 entity list를 생성하고 이를 batch save
-     * @param heroId hero idx
+     * @param batchQuoteInfo batch quote info entity
      * @param results apiResult list (OpenAI response를 parsing한 것)
      * @return void
      */
-    public Mono<Void> insertHeroQuoteKr(String heroId, List<BResultDto.ApiResult> results) {
-        return getLatestHeroQuoteKrIndex(heroId)
+    public Mono<Void> insertHeroQuoteKr(BatchQuoteInfo batchQuoteInfo, List<BResultDto.ApiResult> results) {
+        String heroId = batchQuoteInfo.getHeroId();
+        return heroQuoteKrService.getLatestIndexForHeroQuoteKr(heroId)
                 .flatMap(index ->
                         convertResultToHeroQuoteKr(heroId, index + 1, 1, results)
                 )
@@ -77,15 +97,6 @@ public class BResultFacade {
                         status
                 ))
                 .collectList();
-    }
-
-    /**
-     * heroId로 기존 heroQuoteKr 데이터에서 최신 인덱스 조회
-     * @param heroId hero idx
-     * @return latest index Mono
-     */
-    private Mono<Integer> getLatestHeroQuoteKrIndex(String heroId) {
-        return heroQuoteKrService.findLatestVersionByIdKindSeq(heroId, HERO_QUOTE_KR_KIND, HERO_QUOTE_KR_SEQ);
     }
 
 
